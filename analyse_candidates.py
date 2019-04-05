e56cce254a73449431ce8f2dc2dbc94f6ea24b5a
anrfgd <rahul.anandakkina@bp.com>
2019-04-05 11:20:02 +0800

    Commit for new data

--- analyse_candidates.py
+++ analyse_candidates.py
@@ -4,57 +4,76 @@ import pandas as pd
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
-
+'''Utility Class with few utility methods'''
 class ElectionUtils(object):
+    '''Extracts CSV contents to a dataframe'''
     def create_df(self, file_name):
         return pd.read_csv(file_name, header='infer')
 
+    '''Predicts what a person should be earning given the age bracket (Note : More accurate data in affidavits shall help predicting it better)'''
     def age_earning_df(self, age_idx_df, candidates_data_df):
         for index, row in age_idx_df.iterrows():
             cd_df = candidates_data_df[
                 (candidates_data_df['AGE'] >= row['FROM']) & (candidates_data_df['AGE'] <= row['TO'])]
+
+            #Earnings calculated by the formula EARNINGS = (MOVABLE ASSETS + IMMOVABLE ASSETS) - LIABLITIES
             mean_earnings = ((cd_df['MOVABLE_ASSETS'] + cd_df['IMMOVABLE_ASSETS']) - cd_df['LIABLITIES']).mean()
+
+            #Calculating mean earnings
             age_idx_df.loc[index, 'AVERAGE_EARNINGS'] = mean_earnings
+
+            #Taking boundaries of 50% average for earnings evaluation
             age_idx_df.loc[index, 'MINIMUM_EARNINGS'] = mean_earnings - (mean_earnings * 0.5)
             age_idx_df.loc[index, 'MAXIMUM_EARNINGS'] = mean_earnings + (mean_earnings * 0.5)
+
         return age_idx_df
 
 
 class CandidateDataTransformation(object):
+    #Initilization
     utils = ElectionUtils()
     [candidates_data_df, education_idx_df, weights_df] = [utils.create_df("datasets/CANDIDATES_LIST.csv"),
                                                           utils.create_df("datasets/EDUCATION_INDEX.csv"),
                                                           utils.create_df("datasets/WEIGHTAGE.csv")]
     age_idx_df = utils.age_earning_df(utils.create_df("datasets/AGE_INDEX.csv"), candidates_data_df)
 
+    '''Returns points which can be earned given the age bracket (Please note older people score less through this system)'''
     def get_age_related_points(self, age):
         for index, row in self.age_idx_df.iterrows():
             if row['FROM'] <= age and row['TO'] >= age:
                 return row['POINTS']
 
+    '''Returns points w.r.t number of criminal cases'''
     def get_criminal_case_points(self, pending_cases, convicted_cases):
+        #If convicted we get a negative score twice that of offenses one has committed
         if convicted_cases > 0:
             return -2 * convicted_cases
+        # we get a negative score depending on no. of offenses committed
         elif pending_cases > 0:
             return -1 * pending_cases
         else:
             return 0
 
+    '''Returns points calculated based on education'''
     def get_edu_points(self, education):
         return self.education_idx_df[self.education_idx_df['EDUCATION'] == education]['POINTS'].values[0]
 
+    '''Returns points based on income tax compliance, if you are a regular payer you earn a point'''
     def get_tax_compliance_points(self, tax_compliance):
         dict = {"YES": 1, "NO": 0}
         return dict[tax_compliance]
 
+    '''Returns points based on government dues payout compliance, if you are a regular payer you earn a point'''
     def get_govt_due_points(self, govt_due):
         dict = {"YES": 0, "NO": 1}
         return dict[govt_due]
 
+    '''Returns points based on residency, if the contestent is a local he/she gets a point'''
     def get_local_residency_points(self, local_residency):
         dict = {"YES": 1, "NO": 0}
         return dict[local_residency]
 
+    '''Returns points based on earnings, if he/she earns less or more than average distribution they'd get a 0'''
     def get_earnings_points(self, age, earnings):
 
         for index, row in self.age_idx_df.iterrows():
@@ -64,9 +83,11 @@ class CandidateDataTransformation(object):
                 else:
                     return 1
 
+    '''Gets configured weight'''
     def get_weight(self, key):
         return self.weights_df[self.weights_df['KEY'] == key]['WEIGHT'].values[0]
 
+    '''Evaluates all contestants based on the ratings system and returns a dataframe which is later exported into a CSV for report'''
     def evaluate(self):
 
         for index, row in self.candidates_data_df.iterrows():
@@ -80,6 +101,7 @@ class CandidateDataTransformation(object):
                  self.get_earnings_points(row['AGE'],
                                           ((row['MOVABLE_ASSETS'] + row['IMMOVABLE_ASSETS']) - row['LIABLITIES']))]
 
+            #Weighted sum of all the declared details.
             points_earned = (self.get_weight("AGE") * age_related_points) + (
                 self.get_weight("CRIMINAL_CASES") * criminal_case_points) + (
                                 self.get_weight("EDUCATION") * edu_points) + (
