71313ff4164b9607b7e6d087d294dbb431873440
anrfgd <rahul.anandakkina@bp.com>
2019-04-16 15:58:13 +0800

    Added new data for education related analysis.

--- analyse_candidates.py
+++ analyse_candidates.py
@@ -1,6 +1,7 @@
 import logging
 
 import pandas as pd
+import numpy as np
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
@@ -31,12 +32,13 @@ class ElectionUtils(object):
 
 
 class CandidateDataTransformation(object):
-    #Initilization
+    # Initilization
     utils = ElectionUtils()
     [candidates_data_df, education_idx_df, weights_df, candidate_analysis_df] = [utils.create_df("datasets/CANDIDATES_LIST.csv"),
                                                           utils.create_df("datasets/EDUCATION_INDEX.csv"),
                                                           utils.create_df("datasets/WEIGHTAGE.csv"), utils.create_df("datasets/CANDIDATE_ANALYSED_LIST.csv")]
     age_idx_df = utils.age_earning_df(utils.create_df("datasets/AGE_INDEX.csv"), candidates_data_df)
+
 
     '''Returns points which can be earned given the age bracket (Please note older people score less through this system)'''
     def get_age_related_points(self, age):
@@ -46,7 +48,7 @@ class CandidateDataTransformation(object):
 
     '''Returns points w.r.t number of criminal cases'''
     def get_criminal_case_points(self, pending_cases, convicted_cases):
-        #If convicted we get a negative score twice that of offenses one has committed
+        # If convicted we get a negative score twice that of offenses one has committed
         if convicted_cases > 0:
             return -2 * convicted_cases
         # we get a negative score depending on no. of offenses committed
@@ -58,6 +60,12 @@ class CandidateDataTransformation(object):
     '''Returns points calculated based on education'''
     def get_edu_points(self, education):
         return self.education_idx_df[self.education_idx_df['EDUCATION'] == education]['POINTS'].values[0]
+
+    def get_edu_from_points(self, points):
+        for index, row in self.education_idx_df.iterrows():
+            if row['FROM'] == int(points):
+                return row['EDUCATION']
+        return 'Literate'
 
     '''Returns points based on income tax compliance, if you are a regular payer you earn a point'''
     def get_tax_compliance_points(self, tax_compliance):
@@ -102,7 +110,7 @@ class CandidateDataTransformation(object):
                  self.get_earnings_points(row['AGE'],
                                           ((row['MOVABLE_ASSETS'] + row['IMMOVABLE_ASSETS']) - row['LIABLITIES']))]
 
-            #Weighted sum of all the declared details.
+            # Weighted sum of all the declared details.
             points_earned = (self.get_weight("AGE") * age_related_points) + (
                 self.get_weight("CRIMINAL_CASES") * criminal_case_points) + (
                                 self.get_weight("EDUCATION") * edu_points) + (
@@ -127,24 +135,45 @@ class CandidateDataTransformation(object):
 
         return self.candidates_data_df
 
-    def get_party_criminal_history(self):
+    def calculate_party_criminal_score(self):
         ca_df = self.candidate_analysis_df.groupby(['PARTY']).agg({'CANDIDATE_NAME' : 'count', 'NO_PENDING_CRIMINAL_CASES' : 'sum'}).reset_index().rename(columns={'CANDIDATE_NAME':'NO_CONTESTING_CANDIDATES'})
         ca_df['PENDING_CRIMINAL_CASES_PER_CANDIDATE'] = round((ca_df['NO_PENDING_CRIMINAL_CASES'] / ca_df['NO_CONTESTING_CANDIDATES']),2)
         return ca_df
 
+    def build_candidate_analysis_df(self, df):
+        for index, row in df.iterrows():
+            df.loc[index, 'POINTS_FOR_EDUCATION'] = self.get_edu_points(row['EDUCATION'])
+        return df
 
+    def calculate_party_education_score(self):
+        edu_df = self.build_candidate_analysis_df(self.candidate_analysis_df)
+        edu_df = edu_df.groupby(['PARTY']).agg({'POINTS_FOR_EDUCATION': 'mean'}).reset_index().rename(columns={'POINTS_FOR_EDUCATION':'EDUCATION_INDEX'})
+        edu_df['EDUCATION_INDEX'] = round(edu_df['EDUCATION_INDEX'])
+        return edu_df
 
-export_df = CandidateDataTransformation().evaluate()
+cdt = CandidateDataTransformation()
+
+export_df = cdt.evaluate()
 
 export_df.to_csv("datasets/CONTESTANT_LIST.csv", index=False, header=True)
 
 json_content = export_df.to_json('datasets/CONTESTANT_LIST.json', orient='records')
 
-cc_df = CandidateDataTransformation().get_party_criminal_history()
+logging.info("Exported %d rows to %s file" % (len(export_df), "datasets/CONTESTANT_LIST.csv"))
+
+# Aggregating analysis for party wise candidate recuirtment based on criminal history.
+
+cc_df = cdt.calculate_party_criminal_score()
 
 cc_df.to_csv("datasets/PENDING_CRIMINAL_CASES_BY_PARTY.csv", index=False, header=True)
 
-logging.info("Exported %d rows to %s file" % (len(export_df), "datasets/CONTESTANT_LIST.csv"))
+logging.info("Exported %d rows to %s file" % (len(cc_df), "datasets/PENDING_CRIMINAL_CASES_BY_PARTY.csv"))
+
+edu_df = cdt.calculate_party_education_score()
+
+edu_df.to_csv("datasets/EDUCATION_INDEX_BY_PARTY.csv", index=False, header=True)
+
+logging.info("Exported %d rows to %s file" % (len(edu_df), "datasets/EDUCATION_INDEX_BY_PARTY.csv"))
 
 
 
