879d2f13a7c16003e6122b2c3de1093422991452
anrfgd <rahul.anandakkina@bp.com>
2019-06-18 09:18:25 +0800

    Added new data for education related analysis.

--- train_model.py
+++ train_model.py
@@ -26,7 +26,7 @@ cfg = get_config("config/cfg.json")
 
 df = pd.read_csv(cfg["ML"]["TRAIN"], header='infer')
 
-logging.info(df.shape)
+logging.info("Dataset Dimenstions - Rows : %d, Columns : %d" % (df.shape[0],  df.shape[1]))
 
 x = df.iloc[:, 2:17]
 y = df.iloc[:, 17]
@@ -35,23 +35,22 @@ X = x.values
 Y = y.values
 
 
-
 # Splitting the dataset into the Training set and Test set
 
 logging.info("Splitting data set for Cross Validation ..")
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=2)
 
 
 # Fitting XGBoost to the Training set
 
-xgb = xgboost.XGBRegressor(colsample_bytree=0.8, subsample=0.5, learning_rate=0.05, max_depth=12,
-                           min_child_weight=1, n_estimators=2000, reg_alpha=0.1, reg_lambda=0.2,
-                           gamma=0.01, silent=True, random_state=7, nthread=-1)
+xgb = xgboost.XGBRegressor(colsample_bytree=0.8, subsample=0.5, learning_rate=0.02, max_depth=10,
+                           min_child_weight=1, n_estimators=5000, reg_alpha=0.1, reg_lambda=0.2,
+                           gamma=0.01, silent=True, random_state=7, nthread=-1, missing=None, booster="dart")
 
 logging.info("Fitting XGBoost to the training set ...")
 
-xgb.fit(X_train, Y_train)
+xgb.fit(X_train, Y_train, eval_metric='logloss', verbose=True)
 
 
 # Predicting the Test set results
@@ -62,11 +61,13 @@ Y_pred = xgb.predict(X_test)
 
 Y_train_pred = xgb.predict(X_train)
 
+xgb.save_model(cfg["ML"]["MODEL"])
+
 # Applying k-Fold Cross Validation
 
-logging.info("Applying k-Fold Cross Validation .....")
+logging.info("Applying K-Fold Cross Validation .....")
 
-accuracies = cross_val_score(estimator=xgb, X=X_train, y=Y_train, cv=10)
+accuracies = cross_val_score(estimator=xgb, X=X_train, y=Y_train, cv=10, n_jobs=2, pre_dispatch=4)
 
 logging.info("Accuracies - Mean : %f, Standard Deviation : %f" % (accuracies.mean(), accuracies.std()))
 
