c00423f8094f210c008a4bb7caf975f7d730f9e1
anrfgd <rahul.anandakkina@bp.com>
2019-06-24 20:06:54 +0800

    Reorganizing content

--- python/src/predict.py
+++ python/src/predict.py
@@ -1,16 +1,7 @@
-import pandas as pd
-import logging
-import codecs
-import json
-import numpy as np
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
 import xgboost
-import warnings
+
 
 data = [[2,414,32,187,0,1,0.665,0.031,3,0,-0.0814,1,0,0,0.01]]
-##0.4733, 0.6537
 
 
 xgb = xgboost.XGBRegressor(colsample_bytree=0.8, subsample=0.5, learning_rate=0.01, max_depth=12,
