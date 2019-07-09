3709b89819ec1b5307bc678c1fd2f7403374a1c2
anrfgd <rahul.anandakkina@bp.com>
2019-07-09 13:45:13 +0800

    Added Prediction and other needed services

--- python/src/service/app_svc.py
+++ python/src/service/app_svc.py
@@ -4,6 +4,7 @@ import codecs
 import requests
 import feedparser
 import json
+import xgboost
 import pandas as pd
 from flask import Flask, request
 from flask_restful import Api, Resource
@@ -25,8 +26,9 @@ def get_config(conf_path):
 def create_df(file_name):
     return pd.read_csv(file_name, header='infer')
 
+
 @lru_cache(maxsize=3000)
-def get_media_popularity_score(self, candidate_name):
+def get_media_popularity_score(candidate_name):
     response = requests.get(cfg["NEWS_URL"], params={"q": '"%s"' % candidate_name,
                                              "hl": "en-SG", "gl": "SG", "ceid": "SG:en"}, proxies=cfg['PROXY'])
     if response.status_code == 200:
@@ -41,7 +43,34 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 cfg = get_config("../../config/cfg.json")
 
-candidate_analysis_df = create_df("%s%s" % ("../",cfg["OUTPUT_DATA_SRC"]["CANDIDATE_ANALYSED_LIST"]['CSV']))
+age_df = create_df("%s%s" % ("../", cfg['INPUT_DATA_SRC']['AGE_INDEX']))
+
+candidate_analysis_df = create_df("%s%s" % ("../", cfg["OUTPUT_DATA_SRC"]["CANDIDATE_ANALYSED_LIST"]['CSV']))
+
+xgb = xgboost.XGBRegressor()
+xgb.load_model("%s%s" % ("../", cfg["ML"]["MODEL"]))
+
+
+def get_age_related_points(age):
+    for index, row in age_df.iterrows():
+        if row['FROM'] <= age <= row['TO']:
+            return row['POINTS']
+    return 1
+
+
+def build_age_earnings_dict():
+    dic = {}
+    for idx, r in candidate_analysis_df.iterrows():
+        age_index = r["AGE_GROUP_IDX"]
+        earnings = r["EARNINGS"]
+        if age_index not in dic.keys():
+            dic[age_index] = [0, 0]
+        if earnings > 1:
+            dic[age_index] = [dic[age_index][0] + 1, dic[age_index][1] + earnings]
+    return dic
+
+
+age_earnings_dict = build_age_earnings_dict()
 
 
 '''Gets info about states'''
@@ -105,8 +134,40 @@ class EducationInfo(Resource):
 class Predictor(Resource):
 
     def post(self):
-        content = request.json
-        return content
+        payload = request.json
+        input_vector = self.build_input_vector(payload)
+        score = xgb.predict(input_vector)
+        return {"score": round(float(score[0]), 4)}
+
+    def build_input_vector(self, payload):
+
+        age_points = get_age_related_points(payload["age"])
+        constituency_id = payload["constituencyId"]
+        state_id = payload["stateId"]
+        party_id = payload["partyId"]
+        no_pen_crime_cases = payload["numberOfPendingCriminalCases"]
+
+        [t, s] = age_earnings_dict[age_points]
+        earnings_average = round(s / t, 2)
+        earned_income = payload["earnedIncome"] - payload["liabilities"]
+        [limit_min, limit_max] = [(earnings_average - (earnings_average * 0.5)),
+                                  (earnings_average + (earnings_average * 0.5))]
+
+        earnings_points = 1 if limit_min <= earned_income <= limit_max else 0
+
+        state_literacy_rate = round(payload["stateLiteracyRate"], 4)
+        state_seat_share = round(payload["stateSeatShare"], 4)
+        party_group_id = payload["partyGroupId"]
+        education_group_id = payload["educationGroupId"]
+        delta_voter_turnout = payload["deltaStateVoterTurnout"]
+        number_of_phases = payload["numberOfPhases"]
+        re_contest = 1 if payload["recontest"] else 0
+        sex = 1 if payload["sex"] else 0
+        media_popularity = round(get_media_popularity_score(payload["candidateName"]) / 100.0, 2)
+
+        return [[age_points, constituency_id, state_id, party_id, no_pen_crime_cases, earnings_points,
+                    state_literacy_rate, state_seat_share, party_group_id, education_group_id, delta_voter_turnout,
+                        number_of_phases, re_contest, sex, media_popularity]]
 
 
 app = Flask(__name__)
