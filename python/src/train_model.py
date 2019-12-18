3585ff7c8773a61d26852da7f35684261757bb2c
anrfgd <rahul.anandakkina@bp.com>
2019-12-18 12:53:24 +0800

    Added news search related service

--- python/src/train_model.py
+++ python/src/train_model.py
@@ -18,6 +18,11 @@ def get_config(conf_path):
         return d
 
 
+def rmsle(y_true, y_pred):
+    assert len(y_true) == len(y_pred)
+    return np.sqrt(np.mean(np.power(np.log1p(y_true + 1) - np.log1p(y_pred + 1), 2)))
+
+
 cfg = get_config("../config/cfg.json")
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO, filename=cfg['LOG'])
@@ -26,7 +31,7 @@ warnings.filterwarnings("ignore")
 
 df = pd.read_csv(cfg["ML"]["TRAIN"], header='infer')
 
-logging.info("Dataset Dimenstions - [Rows x Columns] : [%d x %d]" % (df.shape[0],  df.shape[1]))
+logging.info("Dataset Dimensions - [Rows x Columns] : [%d x %d]" % (df.shape[0],  df.shape[1]))
 
 x = df.iloc[:, 2:17]
 y = df.iloc[:, 17]
@@ -34,17 +39,15 @@ y = df.iloc[:, 17]
 X = x.values
 Y = y.values
 
-
 # Splitting the dataset into the Training set and Test set
 
 logging.info("Splitting data set for Cross Validation ..")
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.08, random_state=7)
 
-
 # Fitting XGBoost to the Training set
 
-xgb = xgboost.XGBRegressor(colsample_bytree=0.8, subsample=0.5, learning_rate=0.01, max_depth=8,
+xgb = xgboost.XGBRegressor(colsample_bytree=0.8, subsample=0.5, learning_rate=0.01, max_depth=12,
                            min_child_weight=1, n_estimators=10000, reg_alpha=0.1, reg_lambda=0.2,
                            gamma=0.01, silent=True, random_state=7, nthread=-1, missing=None)
 
@@ -67,14 +70,16 @@ xgb.save_model(cfg["ML"]["MODEL"])
 
 logging.info("Calculating Root Mean Square Error ......")
 
-[train_rmse, test_rmse] = [np.sqrt(mean_squared_error(Y_train, Y_train_pred)),
-                           np.sqrt(mean_squared_error(Y_test, Y_pred))]
+[train_rmse, test_rmse] = [np.sqrt(mean_squared_error(Y_train, Y_train_pred)), np.sqrt(mean_squared_error(Y_test, Y_pred))]
 
 logging.info("RMSE - [train, test] : [%f, %f]" % (train_rmse.round(4), test_rmse.round(4)))
 
-'''logging.info("Applying k-Fold Cross Validation .....")
-accuracies = cross_val_score(estimator=xgb, X=X_train, y=Y_train, cv=10)
+logging.info("Applying k-Fold Cross Validation .....")
 
-logging.info("Accuracies - Mean : %f, Standard Deviation : %f" % (accuracies.mean(), accuracies.std()))'''
+accuracies = cross_val_score(estimator=xgb, X=X_train, y=Y_train, cv=7)
+
+logging.info("Accuracies - Mean : %f, Standard Deviation : %f" % (accuracies.mean(), accuracies.std()))
 
 xgb.save_model(cfg["ML"]["MODEL"])
+
+
