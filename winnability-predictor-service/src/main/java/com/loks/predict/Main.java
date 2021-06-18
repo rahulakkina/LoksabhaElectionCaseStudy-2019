987190afa16de2b66bf2db86f5bd800e36db573b
Rahul Anand Akkina <rahu.anandakkina@bp.com>
2021-06-18 12:08:02 +0800

    Reformatted sources for better readability

--- winnability-predictor-service/src/main/java/com/loks/predict/Main.java
+++ winnability-predictor-service/src/main/java/com/loks/predict/Main.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableScheduling
 public class Main extends SpringApplication{
-   public static void main(String[] args) {
+   public static void main(final String... args) {
        Main.run(Main.class, args);
    }
 }
